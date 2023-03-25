74967513907581b771a9bb0fe388c348b8878906
EL GHAFRAOUI AYOUB <52068209+bbouya@users.noreply.github.com>
2023-03-25 19:03:13 +0000

    c

--- functions1.c
+++ functions1.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /************************* PRINT UNSIGNED NUMBER *************************/
 /**
  * print_unsigned - Prints an unsigned number
@@ -10,6 +11,7 @@
  * @size: Size specifier
  * Return: Number of chars printed.
  */
+
 int print_unsigned(va_list types, char buffer[],
 int flags, int width, int precision, int size)
 {
