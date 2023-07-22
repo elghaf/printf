5951fe63b28392f6bb21a897f931927d1c88b090
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-07-22 22:33:15 +0100

    Update 1-printf.c

--- 1-printf.c
+++ 1-printf.c
@@ -1,46 +1,66 @@
 #include "main.h"
 
+void print_buffer(char buffer[], int *buff_ind);
+
 /**
- * _printf - Printf functions.
- * @format: is a character string.
- * Return: The number of characters printed.
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
  */
 int _printf(const char *format, ...)
 {
-	int result_sum = 0;
-	va_list var_list;
-	char *ch, *st;
+    int i, printed = 0, printed_chars = 0;
+    int flags, width, precision, size, buff_ind = 0;
+    va_list list;
+    char buffer[BUFF_SIZE];
 
-	params_t parameter = PARAMS_INIT;
-	var_start(var_list, format);
-	if ((!format || (format[0] == '%' && !format[1])) || (format[0] == '%' && format[1] == ' ' && !format[2]))
-	return (-1);
-	ch = (char *)format;
-	for (ch ; *ch; ch++)
-	{
-		init_params(&parameter, var_start);
-		if (*ch != '%')/*checking for the % specifier*/
-		{
-			result_sum += _putchar(*ch);
-			continue;
-		}
-		st = ch;
-		ch++;
-		while (get_flag(ch, &parameter)) /* while char at p is flag character */
-		{
-			ch++; /* next character */
-		}
-		ch = get_width(ch, &parameter, var_start);
-		ch = get_precision(ch, &parameter, var_start);
-		if (get_modifier(ch, &parameter))
-			ch++;
-		if (!get_specifier(ch))
-			result_sum += print_from_to(st, ch,
-					parameter.l_modifier || parameter.h_modifier ? ch - 1 : 0);
-		else
-			result_sum += get_print_func(ch, var_start, &parameter);
-	}
-_putchar(BUF_FLUSH);
-va_end(var_list);
-return (result_sum);
+    if (format == NULL)
+        return (-1);
+
+    va_start(list, format);
+
+    for (i = 0; format && format[i] != '\0'; i++)
+    {
+        if (format[i] != '%')
+        {
+            buffer[buff_ind++] = format[i];
+            if (buff_ind == BUFF_SIZE)
+                print_buffer(buffer, &buff_ind);
+            /* write(1, &format[i], 1);*/
+            printed_chars++;
+        }
+        else
+        {
+            print_buffer(buffer, &buff_ind);
+            flags = get_flags(format, &i);
+            width = get_width(format, &i, list);
+            precision = get_precision(format, &i, list);
+            size = get_size(format, &i);
+            ++i;
+            printed = handle_print(format, &i, list, buffer,
+                                   flags, width, precision, size);
+            if (printed == -1)
+                return (-1);
+            printed_chars += printed;
+        }
+    }
+
+    print_buffer(buffer, &buff_ind);
+
+    va_end(list);
+
+    return (printed_chars);
+}
+
+/**
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+    if (*buff_ind > 0)
+        write(1, &buffer[0], *buff_ind);
+
+    *buff_ind = 0;
 }
