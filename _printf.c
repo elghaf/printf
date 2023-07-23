ea95adb41e3a6c47854340bb0df305729a0ea923
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-07-23 22:23:22 +0100

    add

--- _printf.c
+++ _printf.c
@@ -1,81 +1,69 @@
 #include <stdio.h>
-#include <stdarg.h>
-#include "main.h"
 #include <string.h>
+#include <stdarg.h>
+#include <stdbool.h>
 
 /**
  * _printf - Custom implementation of printf with limited conversion specifiers.
  * @format: The format string containing zero or more directives.
  *
- * This function parses the format string and handles the conversion specifiers:
- * 'c' - for printing characters.
- * 's' - for printing strings.
- * '%' - for printing the '%' character.
- *
  * Return: The number of characters printed (excluding the null byte used to end
- * output to strings).
+ *         output to strings).
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    va_start(args, format);
+	va_list args;
+	int index = 0;
+	int inner_index;
+	int chars_printed = 0;
+	char ch;
 
-    int chars_printed = 0;
-    char c;
-    while ((c = *format++))
-    {
-        if (c == '%')
-        {
-            // Get the next character after '%'
-            c = *format++;
-            switch (c)
-            {
-                case 'c':
-                    // Print a character
-                    print_putchar(va_arg(args, int));
-                    chars_printed++;
-                    break;
-                case 's':
-                {
-                    // Print a string
-                    const char *str = va_arg(args, const char*);
-                    while (*str)
-                    {
-                        print_putchar(*str++);
-                        chars_printed++;
-                    }
-                    break;
-                }
-                case '%':
-                    // Print '%'
-                    print_putchar('%');
-                    chars_printed++;
-                    break;
-                default:
-                    // Invalid conversion specifier, ignore and continue
-                    break;
-            }
-        }
-        else
-        {
-            // Regular character, just print it
-            print_putchar(c);
-            chars_printed++;
-        }
-    }
+	va_start(args, format);
 
-    va_end(args);
-    return chars_printed;
-}
+	while (format[index] != '\0')
+	{
+		if (format[index] == '%')
+		{
+			index++;
 
-/**
- * print_putchar - print character.
- * @c: char to print.
- * Return: int 1.
- */
+			if (format[index] == '%')
+			{
+				_putchar('%');
+				chars_printed++;
+			}
+			else if (format[index] == 'c')
+			{
+				ch = va_arg(args, int);
+				_putchar(ch);
+				chars_printed++;
+			}
+			else if (format[index] == 's')
+			{
+				char *str = va_arg(args, char *);
 
-int print_putchar(char c)
-{
-	_putchar(c);
-	return (1);
+				inner_index = 0;
+				while (str[inner_index] != '\0')
+				{
+					_putchar(str[inner_index]);
+					chars_printed++;
+					inner_index++;
+				}
+			}
+			else
+			{
+				_putchar('%');
+				_putchar(format[index]);
+				chars_printed += 2;
+			}
+		}
+		else
+		{
+			_putchar(format[index]);
+			chars_printed++;
+		}
+		index++;
+	}
+
+	va_end(args);
+	return chars_printed;
 }
