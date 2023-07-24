234120778ea2c01047da7e2e3c81f69b1f44674d
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-07-24 16:38:08 +0100

    addd

--- main.h
+++ main.h
@@ -1,22 +1,14 @@
-#ifndef MYHEADER_H
-#define MYHEADER_H
+#ifndef MAIN_H
+#define MAIN_H
 
-#include <stdarg.h>
 #include <stdio.h>
 #include <unistd.h>
-#include <limits.h>
-#include <stdlib.h>
-#include <string.h>
+#include <stdarg.h>
 
+int _strlen(char *s);
+int print_str(va_list args);
+void print_char(va_list args);
 int _printf(const char *format, ...);
-
-/* Funtions to print chars and strings */
-int print_char(va_list);
-int print_string(va_list);
-int print_percent(va_list);
-
-/* Functions to print numbers */
-int print_int(va_list);
-int print_binary(va_list);
+int print_int(va_list args);
 
 #endif
