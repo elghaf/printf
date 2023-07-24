9ec90fae7be2e9d47fc916eff9e01124c059f559
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-07-24 23:45:36 +0100

    Update print_functions.c

--- print_functions.c
+++ print_functions.c
@@ -1,43 +1,60 @@
 #include <stdarg.h>
 #include "main.h"
 
-/* Function implementations for handling each conversion specifier */
-
-int print_char(va_list args)
+/**
+ * print_single_char - Print a single character
+ * @args: A va_list containing the character to be printed
+ *
+ * Return: The number of characters printed (always 1)
+ */
+int print_single_char(va_list args)
 {
-	char c = (char)va_arg(args, int);
-	_putchar(c);
+	char character = (char)va_arg(args, int);
+
+	_putchar(character);
 	return (1);
 }
 
+/**
+ * print_string - Print a string of characters
+ * @args: A va_list containing the string to be printed
+ *
+ * Return: The number of characters printed
+ */
 int print_string(va_list args)
 {
 	const char *str = va_arg(args, const char *);
-	int count = 0;
+	int character_count = 0;
 
 	while (*str)
 	{
 		_putchar(*str++);
-		count++;
+		character_count++;
 	}
 
-	return count;
+	return (character_count);
 }
 
-int print_int(va_list args)
+/**
+ * print_integer - Print an integer
+ * @args: A va_list containing the integer to be printed
+ *
+ * Return: The number of characters printed
+ */
+int print_integer(va_list args)
 {
-	int num = va_arg(args, int);
+	int number = va_arg(args, int);
 	int is_negative = 0;
-	int count = 0;
+	int character_count = 0;
 	int divisor = 1;
-        int temp = num;
+	int temp = number;
 
-	if (num < 0)
+	if (number < 0)
 	{
 		_putchar('-');
-		count++;
+		character_count++;
 		is_negative = 1;
-		num = -num;
+		number = -number;
 	}
 
 	while (temp > 9)
@@ -48,22 +65,28 @@ int print_int(va_list args)
 
 	while (divisor > 0)
 	{
-		int digit = num / divisor;
+		int digit = number / divisor;
 		_putchar('0' + digit);
-		count++;
-		num %= divisor;
+		character_count++;
+		number %= divisor;
 		divisor /= 10;
 	}
 
-	return (count + is_negative);
+	return (character_count + is_negative);
 }
 
-int print_unsigned(va_list args)
+/**
+ * print_unsigned_integer - Print an unsigned integer
+ * @args: A va_list containing the unsigned integer to be printed
+ *
+ * Return: The number of characters printed
+ */
+int print_unsigned_integer(va_list args)
 {
-	unsigned int num = va_arg(args, unsigned int);
+	unsigned int number = va_arg(args, unsigned int);
 	unsigned int divisor = 1;
-	unsigned int temp = num;
-	int count = 0;
+	unsigned int temp = number;
+	int character_count = 0;
 
 	while (temp > 9)
 	{
@@ -73,110 +96,127 @@ int print_unsigned(va_list args)
 
 	while (divisor > 0)
 	{
-		unsigned int digit = num / divisor;
+		unsigned int digit = number / divisor;
 		_putchar('0' + digit);
-		count++;
-		num %= divisor;
+		character_count++;
+		number %= divisor;
 		divisor /= 10;
 	}
 
-	return count;
+	return (character_count);
 }
 
-int print_octal(va_list args)
+/**
+ * print_octal_integer - Print an unsigned integer in octal format
+ * @args: A va_list containing the unsigned integer to be printed
+ *
+ * Return: The number of characters printed
+ */
+int print_octal_integer(va_list args)
 {
-	unsigned int num = va_arg(args, unsigned int);
+	unsigned int number = va_arg(args, unsigned int);
 	unsigned int octal_digits[100];
 	int octal_count = 0;
-	int count = 0;
+	int character_count = 0;
 	int i;
 
-	if (num == 0)
+	if (number == 0)
 	{
 		_putchar('0');
-		count++;
+		character_count++;
 	}
 	else
 	{
-		while (num > 0)
+		while (number > 0)
 		{
-			octal_digits[octal_count++] = num % 8;
-			num /= 8;
+			octal_digits[octal_count++] = number % 8;
+			number /= 8;
 		}
 
 		for (i = octal_count - 1; i >= 0; i--)
 		{
 			_putchar('0' + octal_digits[i]);
-			count++;
+			character_count++;
 		}
 	}
 
-	return count;
+	return (character_count);
 }
 
-int print_hex(va_list args)
+/**
+ * print_hexadecimal - Print an unsigned integer in hexadecimal format
+ * @args: A va_list containing the unsigned integer to be printed
+ *
+ * Return: The number of characters printed
+ */
+int print_hexadecimal(va_list args)
 {
-	unsigned int num = va_arg(args, unsigned int);
+	unsigned int number = va_arg(args, unsigned int);
 	char hex_digits[] = "0123456789abcdef";
 	int hex_count = 0;
 	char buffer[100];
-	int count = 0;
+	int character_count = 0;
 	int i;
 
-	if (num == 0)
+	if (number == 0)
 	{
 		_putchar('0');
-		count++;
+		character_count++;
 	}
 	else
 	{
-		while (num > 0)
+		while (number > 0)
 		{
-			buffer[hex_count++] = hex_digits[num % 16];
-			num /= 16;
+			buffer[hex_count++] = hex_digits[number % 16];
+			number /= 16;
 		}
 
 		for (i = hex_count - 1; i >= 0; i--)
 		{
 			char digit = buffer[i];
 			_putchar(digit);
-			count++;
+			character_count++;
 		}
 	}
 
-	return count;
+	return (character_count);
 }
 
-int print_hex_upper(va_list args)
+/**
+ * print_hexadecimal_uppercase - Print an unsigned integer in uppercase hexadecimal format
+ * @args: A va_list containing the unsigned integer to be printed
+ *
+ * Return: The number of characters printed
+ */
+int print_hexadecimal_uppercase(va_list args)
 {
-	unsigned int num = va_arg(args, unsigned int);
+	unsigned int number = va_arg(args, unsigned int);
 	char hex_digits[] = "0123456789ABCDEF";
 	int hex_count = 0;
 	char buffer[100];
-	int count = 0;
+	int character_count = 0;
 	int i;
 
-	if (num == 0)
+	if (number == 0)
 	{
 		_putchar('0');
-		count++;
+		character_count++;
 	}
 	else
 	{
-		while (num > 0)
+		while (number > 0)
 		{
-			buffer[hex_count++] = hex_digits[num % 16];
-			num /= 16;
+			buffer[hex_count++] = hex_digits[number % 16];
+			number /= 16;
 		}
 
 		for (i = hex_count - 1; i >= 0; i--)
 		{
 			char digit = buffer[i];
 			_putchar(digit);
-			count++;
+			character_count++;
 		}
 	}
 
-	return count;
+	return (character_count);
 }
-
