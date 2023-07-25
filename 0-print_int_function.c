77bcb33b39eb5e14dc5e184a9029e89aa298bde3
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-07-25 21:33:13 +0100

    Update 0-print_int_function.c

--- 0-print_int_function.c
+++ 0-print_int_function.c
@@ -4,46 +4,46 @@
 #include "main.h"
 /**
  * print_int - The function print integer.
- *@num: The integer to be printed
- *@sign: The negative or positve sign
- *@space: The space between the integer
+ *@integer_print: The integer to be printed
+ *@neg_pos: The negative or positve sign
+ *@integer_space: the integer space
  *
  *Return: Return index_counter.
  */
 
-int print_int(int num, char sign, char space)
+int print_int(int integer_print, char neg_pos, char integer_space)
 {
 	int index_counter = 0;
 	char buffer[12];
 	int len = 0;
 
-	if (num == INT_MIN)
+	if (integer_print == INT_MIN)
 	{
 		index_counter += write(1, "-2147483648", 11);
 		return (index_counter);
 	}
 
-	if (num < 0)
+	if (integer_print < 0)
 	{
 		index_counter += write(1, "-", 1);
-		num = -num;
-	} else if (sign)
+		integer_print = -integer_print;
+	} else if (neg_pos)
 	{
 		index_counter += write(1, "+", 1);
-	} else if (space)
+	} else if (integer_space)
 	{
 		index_counter += write(1, " ", 1);
 	}
-	else if (num == 0)
+	else if (integer_print == 0)
 	{
 		index_counter += write(1, "0", 1);
 		return (index_counter);
 	}
 
 	do {
-		buffer[len++] = '0' + num % 10;
-		num /= 10;
-	} while (num > 0);
+		buffer[len++] = '0' + integer_print % 10;
+		integer_print /= 10;
+	} while (integer_print > 0);
 
 	while (len > 0)
 		index_counter += write(1, &buffer[--len], 1);
