46f79dc455d9e2deb5dd2b5e198b9207c65caf14
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-07-25 17:50:50 +0100

    oo

--- main.h
+++ main.h
@@ -1,9 +1,26 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef MY_PRINTF
+#define MY_PRINTF
 
-#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdint.h>
 
+
+struct convert
+{
+	char *sym;
+	int (*f)(va_list);
+};
+typedef struct convert conver_t;
+
+
+int _putchar(char c);
 int _printf(const char *format, ...);
-int m_putchars(char c);
+int p_char(va_list);
+int p_string(va_list args);
+int p_percent(va_list);
+int helper(int *count, const char *format, va_list args);
+int print_int(int num, char sign, char space);
 
 #endif
