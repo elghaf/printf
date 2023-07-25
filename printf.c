8dc44f1513596b9cc76e460fc421bdfff14ac208
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-07-26 00:05:26 +0100

    Update printf.c

--- printf.c
+++ printf.c
@@ -42,7 +42,7 @@ int print_string(const char *str)
  * Return: return -1 otherwise no return
  */
 
-int need(int *count, const char *format, va_list args)
+int helper(int *count, const char *format, va_list args)
 {
 	char *str;
 	char ch, percent = '%';
@@ -111,7 +111,7 @@ int _printf(const char *format, ...)
 		if (*format == '%')
 		{
 			format++;
-			need(&index_counter, format, arguments);
+			helper(&index_counter, format, arguments);
 		}
 		else
 		{
