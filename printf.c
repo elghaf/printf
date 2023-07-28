d491d896bb2850d3a8833f74c77b778602be826a
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-07-28 18:41:22 +0100

    Update printf.c

--- printf.c
+++ printf.c
@@ -112,8 +112,7 @@ int _printf(const char *format, ...)
 		if (*format == '%')
 		{
 			/*helper function to define the format of char*/
-			helper(&index_counter, format, arguments);
-			format++;
+			helper(&index_counter, format++, arguments);
 		}
 		else
 		{
