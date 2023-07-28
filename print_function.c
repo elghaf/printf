37b55739440f9162b48c693814a85ffbd0c18bc9
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-07-28 22:16:01 +0100

    Update print_function.c

--- print_function.c
+++ print_function.c
@@ -28,15 +28,19 @@ int print_numbers(int integer_print, char neg_pos, char integer_space)
 	{
 		index_counter = index_counter + write(1, "-", 1);
 		i++;
-		integer_print = -integer_print;
+		integer_print = - (integer_print);
 	}
 	if (neg_pos)
 	{
+		i++;
+		string_len++;
 		index_counter = index_counter + write(1, "+", 1);
 	}
 	if (integer_print == 0)
 	{
 		index_counter = index_counter + write(1, "0", 1);
+		i++;
+		string_len++;
 		return (index_counter);
 	}
 	if (integer_space)
